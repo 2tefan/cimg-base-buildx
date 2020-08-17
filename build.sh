7ce5d470c693adad6a2cc2fcd7e73212ebd4bba8
2tefan <2tefan@protonmail.com>
2020-08-17 13:37:50 +0000

    Added appendtag

--- build.sh
+++ build.sh
@@ -3,17 +3,21 @@
 _CIMG_VERSION=stable-18.04
 _BUILDX_VERSION=0.4.1
 _DOCKER_REPOSITORY="2tefan/cimg-base-buildx"
+_APPEND_TAG=""
 
 PUSH=NO
 
 if [ -z "${CIMG_VERSION}" ]; then
    CIMG_VERSION="${_CIMG_VERSION}"
 fi
-if [ -z "${_BUILDX_VERSION}" ]; then
+if [ -z "${BUILDX_VERSION}" ]; then
    BUILDX_VERSION="${_BUILDX_VERSION}"
 fi
 if [ -z "${DOCKER_REPOSITORY}" ]; then
    DOCKER_REPOSITORY="${_DOCKER_REPOSITORY}"
+fi
+if [ -z "${APPEND_TAG}" ]; then
+   APPEND_TAG="${_APPEND_TAG}"
 fi
 
 for arg in "$@"; do
@@ -29,8 +33,12 @@ for arg in "$@"; do
         BUILDX_VERSION="${arg#*=}"
         shift
         ;;
-    --dockerrepo=* | --docker-repository=* | --dr=* | DOCKER_REPOSITORY=*)
+    --dockerrepo=* | --docker-repository=* | --dr=* | --DOCKER_REPOSITORY=*)
         DOCKER_REPOSITORY="${arg#*=}"
+        shift
+        ;;
+    --appendtag=* | --append-tag=* | --at=* | --APPEND_TAG=*)
+        APPEND_TAG="${arg#*=}"
         shift
         ;;
     esac
@@ -39,9 +47,9 @@ done
 docker build \
     --build-arg CIMG_VERSION=${CIMG_VERSION} \
     --build-arg BUILDX_VERSION=${BUILDX_VERSION} \
-    --tag ${DOCKER_REPOSITORY}:${CIMG_VERSION}-${BUILDX_VERSION} .
+    --tag ${DOCKER_REPOSITORY}:${CIMG_VERSION}-${BUILDX_VERSION}${APPEND_TAG} .
 
 if [ ${PUSH} = "YES" ]; then
-    docker login --username "${DOCKER_USERNAME}" --pasword "${DOCKER_PASSWORD}"
-    docker push ${DOCKER_REPOSITORY}:${CIMG_VERSION}-${BUILDX_VERSION}
+    docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
+    docker push ${DOCKER_REPOSITORY}:${CIMG_VERSION}-${BUILDX_VERSION}${APPEND_TAG}
 fi

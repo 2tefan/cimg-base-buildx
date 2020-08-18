f12d62d06f9feef8373d67b7492b662529796b07
2tefan <2tefan@protonmail.com>
2020-08-18 08:56:40 +0000

    Don't delete master

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,14 @@
 
 . $(dirname $0)/common.sh
 
-curl -i -X DELETE \
-                 -H "Accept: application/json" \
-                 -H "Authorization: JWT ${HUB_TOKEN}" \
-                 https://hub.docker.com/v2/repositories/${DOCKER_REPOSITORY}/tags/${CIMG_VERSION}-${BUILDX_VERSION}${APPEND_TAG}/
+case "${BRANCH}" in
+master)
+    echo "Master stays!"
+    ;;
+*)
+    curl -i -X DELETE \
+        -H "Accept: application/json" \
+        -H "Authorization: JWT ${HUB_TOKEN}" \
+        https://hub.docker.com/v2/repositories/${DOCKER_REPOSITORY}/tags/${CIMG_VERSION}-${BUILDX_VERSION}${APPEND_TAG}/
+    ;;
+esac
